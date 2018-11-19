54f87ef4f705292b23e36d33d463f13b4249f3f3
Kelvin Hoover <kelvin.hoover@hotmail.com>
2018-11-19 10:55:38 -0800

    Checkpoint - before switching to provider model

--- Src/Dev/Ledger/LedgerMenu/Pages/Index.cshtml.cs
+++ Src/Dev/Ledger/LedgerMenu/Pages/Index.cshtml.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LedgerMenu.Pages
 {
+    [AllowAnonymous]
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGetAsync()
         {
+            if(User.Identity.IsAuthenticated)
+            {
+                return RedirectToPage("/Main");
+            }
 
+            return Page();
         }
     }
 }
